c768fef6977aa1f5380b859f2ebd28e46a6a1ad1
Abhirukth Kumar Chakravarthy <55754328+abhirukth@users.noreply.github.com>
2020-10-04 19:58:07 +0530

    Add files via upload

---
+++ MAD LIBS game/mad_libs.s
@@ -0,0 +1,52 @@
+	.data
+welcome:		.asciiz		"\t\t\tWELCOME TO THE GAME OF MAD-LIBS!\n\n"
+phrase_1:		.asciiz		"I met a / an "
+phrase_2:		.asciiz		"When I saw "
+phrase_3:		.asciiz		"I asked, How is your "
+ending:			.asciiz		"\n\t\t\t NOW READ AND ENJOY!\n\t\t\tTHANK YOU FOR PLAYING!\n"
+space:			.space		500
+	.text
+main:
+	li		$v0,	4						# Prints the message.
+	la		$a0,	welcome
+	syscall
+	
+	la		$a0,	phrase_1
+	syscall
+	
+							# Input string.
+	
+							# Loading the space given to it in the register.
+	
+	
+	li		$v0,	4
+	la		$a0,	phrase_2
+	syscall
+	
+							# Input string.
+	
+							# Loading the space given to it in the register.
+	
+	
+	
+	li		$v0,	4
+	la		$a0,	phrase_3
+	syscall
+	
+							# Input string.
+	
+							# Loading the space given to it in the register.
+	
+	
+	li		$v0,	4
+	la		$a0,	ending
+	syscall
+	
+	li		$v0,	10						# Termination call.
+	syscall
+	
+.end main
+
+
+			# Sky is the limit!!! :D........make your own story
+			# It's your life. Make it large ;)
