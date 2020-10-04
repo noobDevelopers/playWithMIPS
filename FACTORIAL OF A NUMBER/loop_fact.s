b9ef415a15a80c8603eee84a1a09f3ca1d78886d
Ritesh Bhaskar <56996641+rbhaskar7@users.noreply.github.com>
2020-10-04 19:46:50 +0530

    Add files via upload

---
+++ FACTORIAL OF A NUMBER/loop_fact.s
@@ -0,0 +1,56 @@
+#	.data
+prompt: .asciiz "ENTER ANY INTEGER: "
+res_1: .asciiz "THE FACTORIAL OF "
+res_2: .asciiz " is "
+res_3: .asciiz ".\n"
+	.text
+main:
+	li $v0, 4
+	la $a0, prompt
+	syscall
+	
+	li $v0, 5
+	syscall
+	move $t0, $v0
+	
+	# Initialising the variables
+	
+	li $t1, 1			# counter variable
+	li $t2, 1			# result variable
+	
+# This loop calculates the factorial
+# for(i = 1, r =1; i <= n; i++)
+#	{
+# 		r *= i;	
+# 	}
+	
+loop_fact:
+		
+								#Implement factorial function (hint: use for loop)
+
+		
+# This loop prints the result		
+		
+print_fact:
+		li $v0, 4
+		la $a0, res_1
+		syscall
+		
+		li $v0, 1
+		move $a0, $t0
+		syscall
+		
+		li $v0, 4
+		la $a0, res_2
+		syscall
+		
+		li $v0, 1
+		move $a0, $t2
+		syscall
+		
+		li $v0, 4
+		la $a0, res_3
+		syscall
+		
+		li $v0, 10
+		syscall
