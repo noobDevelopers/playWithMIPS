246bf54a6e3f5bb651ce812728b89b137e68e47f
Ritesh Bhaskar <56996641+rbhaskar7@users.noreply.github.com>
2020-10-04 21:09:41 +0530

    Update b_d.s

--- Decode the code/b_d.s
+++ Decode the code/b_d.s
@@ -1,23 +1,25 @@
 # Correct the following code so that the program converts binary numbers to decimal, and can work perfectly.
 
+# L-45
+#********************************************************************************************************************************
 		.data
 msg:	.asciiz "Enter binary code: "
 dec:	.asciiz "\nDecoded number: "
 
 		.text
 main:	li $v0,	4
-		la $a0, msg
-		syscall
+	la $a0, msg
+	syscall
 
-		li $v0,	5
-		syscall
-		move $t0, $v0
+	li $v0,	5
+	syscall
+	move $t0, $v0
 
-		li $s0, 0
-		li $s1, 10
-		li $s4, 0.5
-		li $s5, 2
-		li $t2, 0
+	li $s0, 0
+	li $s1, 10
+	li $s4, 0.5
+	li $s5, 2
+	li $t2, 0
 
 conv:	beq $t0, $s0, cont
 		div $t0, $s1
@@ -38,8 +40,31 @@ cont:	li $v0, 4
 
 		li $v0, 10
 		syscall
+		
+#********************************************************************************************************************************
+# This program was developed by a noob.....as you are a "NOOB" , help this noob to correct the incorrect program and make him feel proud!!
+
+# Correct the program to make it run.
 
 
-				# This program was developed by a noob.....as you are a "NOOB" , help this noob to correct the incorrect program and make him feel proud!!
 
-				# Correct the program to make it run.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#********************************************************************************************************************************
