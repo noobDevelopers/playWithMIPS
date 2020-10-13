8e56df34971046ed3431221f472ae2578cc4c32d
rbhaskar7 <56996641+rbhaskar7@users.noreply.github.com>
2020-10-13 19:21:38 +0530

    CHANGES IN FACTORIAL.

--- FACTORIAL OF A NUMBER/loop_fact.s
+++ FACTORIAL OF A NUMBER/loop_fact.s
@@ -1,13 +1,10 @@
 # Complete the loop_fact so that the program computes the factorial of any number
 
-# L-33
-#********************************************************************************************************************************
-
 	.data
 prompt: .asciiz "ENTER ANY INTEGER: "
-res_1: .asciiz "THE FACTORIAL OF "
-res_2: .asciiz " is "
-res_3: .asciiz ".\n"
+res_1: 	.asciiz "THE FACTORIAL OF "
+res_2: 	.asciiz " is "
+res_3: 	.asciiz ".\n"
 	.text
 main:
 	li $v0, 4
@@ -20,26 +17,26 @@ main:
 	
 	# Initialising the variables
 	
-	li $t1, 1			# counter variable
-	li $t2, 1			# result variable
+	li $t1, 1			# Counter variable.
+	li $t2, 1			# Result variable.
 	li $t3, 0			
-	ble $t0, $t3, print_fact			# base case
+	
+	ble $t0, $t3, 	print_fact			# Base case.
+
 # This loop calculates the factorial
 # for(i = 1, r =1; i <= n; i++)
 #	{
 # 		r *= i;	
 # 	}
-
-#********************************************************************************************************************************		
-#Implement factorial function (hint: use for loop)
+	
+# Implement factorial function (hint: use for loop)
 		
 loop_fact:
-	mul $t2, $t2, $t1							#result*=counter
-	addi $t1, 1									#counter+=1
-	ble $t1, $t0, loop_fact						#if counter<=input, continue with loop
-	bgt $t1, $t0, print_fact					#if counter>input, exit loop 
+	mul $t2, $t2, $t1							# Result *= counter.
+	addi $t1, 1									# Counter += 1
+	ble $t1, $t0, loop_fact						# If counter <= input, continue with loop.
+	bgt $t1, $t0, print_fact					# If counter > input, exit loop.
 		
-#********************************************************************************************************************************
 # This loop prints the result		
 		
 print_fact:
