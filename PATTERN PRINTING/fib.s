8032d6556cd16a862b13cb8778db3738ceea4186
SlokAks <slok.aks204@gmail.com>
2020-10-16 01:04:17 +0530

    PATTERN PRINTING code completed

--- PATTERN PRINTING/fib.s
+++ PATTERN PRINTING/fib.s
@@ -7,6 +7,7 @@
 terms:		.asciiz		"ENTER THE NUMBER OF TERMS: "
 fibo:		.asciiz		"THE CORRESPONDING FIBONACCI SERIES IS: "
 space:		.asciiz		" "
+triangle:	.asciiz		"\n\t"
 	.text
 main:
 	li		$v0,	4							# Printing out the message.
@@ -41,17 +42,24 @@ main:
 	fibonacci:
 		
 	#Implement fibonacci function (hint: use recurrsion)
-
-
-
-
-
-
-
-
-
-
+	li		$v0,	4							# Printing out the space.
+	la 		$a0,	space
+	syscall
+	
+	li		$v0,	1							# Print fibonacci term.
+	move		$a0, 	$t1
+	syscall
+	
+	addi	$t3, 	$t3, 	1							# Update counter variable.
+	  
+	add	$t4,	$t1,	$t2							# nextterm = t1 + t2
+	move	$t1,	$t2								# t1 = t2
+	move 	$t2, 	$t4								# t2 = nextTerm
+	
+	ble	$t3, 	$t0, 	fibonacci						# Checking condition i<=n	i:$t3	n:$t0
 	#********************************************************************************************************************************
+	
+	jal pattern									# Call pattern subroutine
 	
 	# Ending of program.
 	terminate:
@@ -71,9 +79,57 @@ main:
 	pattern:
 
 	#Implement pattern function (hint: use for loop)
-
-
-
+	
+	# Initialising the variables needed.
+	li		$t1,	0							# First term = 0.
+	li		$t2,	1							# Second term = 1.
+	li		$t4,	0							# Next term of series.
+	
+	li		$t3,	1							# Counter variable reset to 1.
+	li		$t5,	1							# Number of columns in row.
+	li 		$t6,	0							# Counter for row printing.
+	
+	li		$v0,	4							# Printing out nextline.
+	la 		$a0,	triangle
+	syscall
+	
+	loop:
+		li		$v0,	4						# Printing out the space.
+		la 		$a0,	space
+		syscall
+	
+		li		$v0,	1						# Print fibonacci term.
+		move		$a0, 	$t1
+		syscall
+		
+		addi	$t6,	$t6, 	1						# Update counter variable for row.
+		
+		beq 	$t5,	$t6, 	nextline
+		j continue
+		
+		nextline:
+		
+			li		$v0,	4					# Printing out nextline.
+			la 		$a0,	triangle
+			syscall
+			
+			addi	$t5,	$t5,	1					# Increment number of column per row
+			li 		$t6,	0					# Reset counter for row printing.
+			
+		continue:
+		
+			addi	$t3, 	$t3, 	1					# Update counter variable.
+	  
+			add	$t4,	$t1,	$t2					# nextterm = t1 + t2
+			move	$t1,	$t2						# t1 = t2
+			move 	$t2, 	$t4						# t2 = nextTerm
+	
+			ble	$t3, 	$t0, 	loop					# Checking condition i<=n	i:$t3	n:$t0
+		
+		
+	exit:
+		
+		jr $ra
 
 
 
