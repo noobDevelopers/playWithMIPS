97f0dbc289d1bb67a0fa05c502d4bfe77edc2408
rbhaskar7 <56996641+rbhaskar7@users.noreply.github.com>
2020-10-25 16:12:38 +0530

    UPDATED THE CALCULATOR

--- A SIMPLE CALCULATOR/calc.s
+++ A SIMPLE CALCULATOR/calc.s
@@ -1,10 +1,5 @@
 # Complete the addition, subtraction, multiplication, and division functions, so that the program works as a calculator.
 
-# L-32
-# L-45
-# L-86
-
-#********************************************************************************************************************************
 	.data
 num_1:		.asciiz		"ENTER 1st NUMBER: "
 num_2:		.asciiz		"ENTER 2nd NUMBER: "
@@ -28,29 +23,17 @@ main:
 	la		$a0,	num_1
 	syscall
 	
-	#**********************************************************************************************************************
-	# Input 1st integer.
+	li		$v0,	5							# Input integer.
+	syscall
+	move	$t0,	$v0
 	
-	
-	
-	
-	
-	
-	
-	#**********************************************************************************************************************
 	li		$v0,	4
 	la		$a0,	num_2
 	syscall
-	#**********************************************************************************************************************
-	# Input 2st integer.
 	
-	
-	
-	
-	
-	
-	
-	#**********************************************************************************************************************
+	li		$v0,	5
+	syscall
+	move	$t1,	$v0
 		
 	# Menu for different operations.	
 	menu:
@@ -82,67 +65,133 @@ main:
 	la		$a0,	new_line
 	syscall
 	
-	#********************************************************************************************************************************
 	# Addition Functon.
 	addition:
+		bne		$t2,	1,		subtraction			# If $t2 != 1 then goto subtraction.
 		
-	#Implement addition function (hint: use for loop)
-
-
-
-
-
-
-
-
-
-
+		add		$t3,	$t0,	$t1					# $t3 = $t0 + $t1.
+		
+		li		$v0,	1							# Prints integer.
+		move	$a0,	$t0
+		syscall
+		
+		li		$v0,	4
+		la		$a0,	sum
+		syscall
+		
+		li		$v0,	1
+		move	$a0,	$t1
+		syscall
+		
+		li		$v0,	4
+		la		$a0,	equals
+		syscall
+		
+		li		$v0,	1
+		move	$a0,	$t3
+		syscall
+		
+		li		$v0,	4
+		la		$a0,	new_line
+		syscall
+		
 		j		menu								# Control goes back to menu function.
 	
 	# Subtraction Functon
 	subtraction:
+		bne		$t2,	2,		multiplication		# If $t2 != 2 then goto multiplication.
 		
-	#Implement subtraction function (hint: use for loop)
-
-
-
-
-
-
-
-
-
+		sub		$t3,	$t0,	$t1					# $t3 = $t0 - $t1.
+		
+		li		$v0,	1
+		move	$a0,	$t0
+		syscall
+		
+		li		$v0,	4
+		la		$a0,	subtract
+		syscall
+		
+		li		$v0,	1
+		move	$a0,	$t1
+		syscall
+		
+		li		$v0,	4
+		la		$a0,	equals
+		syscall
+		
+		li		$v0,	1
+		move	$a0,	$t3
+		syscall
+		
+		li		$v0,	4
+		la		$a0,	new_line
+		syscall
+		
 		j		menu
 		
 	# Multiplication Function
 	multiplication:
+		bne		$t2,	3,		division			# If $t2 != 3 then goto division.
 		
-	#Implement multiplication function (hint: use for loop)
-
-
-
-
-
-
-
-
+		mul		$t3,	$t0,	$t1					# $t3 = $t0 * $t1.
+		
+		li		$v0,	1
+		move	$a0,	$t0
+		syscall
+		
+		li		$v0,	4
+		la		$a0,	multiply
+		syscall
+		
+		li		$v0,	1
+		move	$a0,	$t1
+		syscall
+		
+		li		$v0,	4
+		la		$a0,	equals
+		syscall
+		
+		li		$v0,	1
+		move	$a0,	$t3
+		syscall
+		
+		li		$v0,	4
+		la		$a0,	new_line
+		syscall
+		
 		j		menu
 	
 	# Division Function
 	division:
+		bne		$t2,	4,		new_num				# If $t2 != 4 then goto new_num.
 		
-	#Implement division function (hint: use for loop)
-
-
-
-
-
-
-
-
-
+		div		$t3,	$t0,	$t1					# $t3 = $t0 / $t1.
+		
+		li		$v0,	1
+		move	$a0,	$t0
+		syscall
+		
+		li		$v0,	4
+		la		$a0,	divide
+		syscall
+		
+		li		$v0,	1
+		move	$a0,	$t1
+		syscall
+		
+		li		$v0,	4
+		la		$a0,	equals
+		syscall
+		
+		li		$v0,	1
+		move	$a0,	$t3
+		syscall
+		
+		li		$v0,	4
+		la		$a0,	new_line
+		syscall
+		
 		j		menu
-	#********************************************************************************************************************************
 	
 	# Function To Generate New Numbers.
 	new_num:
